9b9351047694f1c1e83b9649c694ed218cdf7999
Seven <lovevipdsw@outlook.com>
2016-09-30 12:09:07 +0000

    Applied fixes from StyleCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
 if (!class_exists('Medz\\Component\\Filesystem\\Filesystem')) {
-    require file_exists(__DIR__ . '/../../../autoload.php')
-        ? __DIR__ . '/../../../autoload.php'
-        : __DIR__ . '/../vendor/autoload.php'
-    ;
+    require file_exists(__DIR__.'/../../../autoload.php')
+        ? __DIR__.'/../../../autoload.php'
+        : __DIR__.'/../vendor/autoload.php';
 }
